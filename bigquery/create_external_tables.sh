7d080a4f7d012957ab0441963a59f9a95ea73bea
david-doit-intl <david@doit-intl.com>
2021-03-23 11:48:48 +0000

    Chore: Liniting and Hinting
    Signed-off-by: david-doit-intl <david@doit-intl.com>

--- bigquery/create_external_tables.sh
+++ bigquery/create_external_tables.sh
@@ -3,7 +3,7 @@ bq mkdef \
   --source_format=NEWLINE_DELIMITED_JSON \
   --hive_partitioning_mode=AUTO \
   --hive_partitioning_source_uri_prefix=gs://warehouse-ticket-data/groups/ \
-  gs://warehouse-ticket-data/groups/ingest_date=*/groups.json > groups.json
+  gs://warehouse-ticket-data/groups/*/groups.json > groups.json
 
 bq rm -f -t \
  warehouse-302911:stream.groups
@@ -18,8 +18,7 @@ bq mkdef \
   --source_format=NEWLINE_DELIMITED_JSON \
   --hive_partitioning_mode=AUTO \
   --hive_partitioning_source_uri_prefix=gs://warehouse-ticket-data/tickets/ \
-  --require_hive_partition_filter=True \
-  gs://warehouse-ticket-data/tickets/ingest_date=*/tickets.json > tickets.json
+  gs://warehouse-ticket-data/tickets/*/tickets.json > tickets.json
 
 bq rm -f -t \
  warehouse-302911:stream.tickets
@@ -33,8 +32,7 @@ bq mkdef \
   --source_format=NEWLINE_DELIMITED_JSON \
   --hive_partitioning_mode=AUTO \
   --hive_partitioning_source_uri_prefix=gs://warehouse-ticket-data/organizations/ \
-  --require_hive_partition_filter=True \
-  gs://warehouse-ticket-data/organizations/*json > organizations.json
+  gs://warehouse-ticket-data/organizations/*/organizations.json > organizations.json
 
 bq rm -f -t \
  warehouse-302911:stream.organizations
@@ -42,3 +40,18 @@ bq rm -f -t \
 bq mk \
   --external_table_definition=organizations.json \
   stream.organizations
+
+bq mkdef -i \
+  --source_format=NEWLINE_DELIMITED_JSON \
+  --hive_partitioning_mode=AUTO \
+  --hive_partitioning_source_uri_prefix=gs://warehouse-ticket-data/organizations/ \
+  gs://warehouse-ticket-data/users/*/users.json \
+  id:INTEGER,url:STRING,name:STRING,email:STRING,created_at:STRING,updated_at:STRING,time_zone:STRING,iana_time_zone:STRING,phone:STRING,shared_phone_number:BOOLEAN,locale_id:INTEGER,locale:STRING,organization_id:INTEGER,role:STRING,verified:BOOLEAN,external_id:INTEGER,alias:STRING,active:BOOLEAN,shared:BOOLEAN,shared_agent:BOOLEAN,last_login_at:STRING,two_factor_auth_enabled:BOOLEAN,signature:STRING,details:STRING,notes:STRING,role_type:INTEGER,custom_role_id:INTEGER,moderator:BOOLEAN,ticket_restriction:STRING,only_private_comments:BOOLEAN,restricted_agent:BOOLEAN,suspended:BOOLEAN,chat_only:BOOLEAN,default_group_id:INTEGER,report_csv:BOOLEAN > users.json
+
+bq rm -f -t \
+ warehouse-302911:stream.users
+
+bq mk \
+  --external_table_definition=users.json \
+  stream.users
+
